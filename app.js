d01cc84e39de8707e67ab7fcc9176b7c25aa65f2
Howard Ting <howardtingting@gmail.com>
2021-12-10 17:05:20 -0800

    Snapshot images are flipped correctly; snapshots don't freeze camera

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var constraints = {
 
 const cameraView = document.querySelector("#camera--view"),
       cameraOutput = document.querySelector("#camera--output"),
-      cameraSensor = document.querySelector("#camera--sensor"),
+      cameraSnapshot = document.querySelector("#camera--snapshot"),
       cameraTrigger = document.querySelector("#camera--trigger");
 
 function cameraStart() {
@@ -22,15 +22,27 @@ function cameraStart() {
         });
 }
 
+cameraOutput.ontransitionstart = () => {
+    cameraSnapshot.innerHTML="";
+};
+
 cameraTrigger.onclick = function() {
+    cameraOutput.classList.remove("hidden");
+    cameraOutput.classList.remove("taken");
+    cameraSnapshot.innerHTML="";
+    const sensorCanvas = document.createElement("canvas");
+    sensorCanvas.setAttribute("id", "camera--sensor");
+    
+    cameraSnapshot.appendChild(sensorCanvas);
     // canvasing
     // when camera is triggered,
     // 1. draw (frozen picture / cameraView / camera--view) onto canvas
     // 2. spread canvas on top of the video
-    cameraSensor.width = cameraView.videoWidth;
-    cameraSensor.height = cameraView.videoHeight;
-    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    sensorCanvas.width = cameraView.videoWidth;
+    sensorCanvas.height = cameraView.videoHeight;
+    sensorCanvas.getContext("2d").drawImage(cameraView, 0, 0);
+    // 3. Place camera output as a small box on the upper right corner
+    cameraOutput.src = sensorCanvas.toDataURL("image/webp");
     cameraOutput.classList.add("taken");
 };
 
